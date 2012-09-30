344e5f1673c470e67dd28f956085a7534f5a3451
Abishek Bhat <bhat.abishek@gmail.com>
2012-09-30 15:00:04 +0530

    Support for Text-Processing API

--- app.rb
+++ app.rb
@@ -1,14 +1,26 @@
 require 'sinatra'
-
+require 'json'
+require 'net/http'
+require 'uri'
 get '/' do 
 
 erb :index
 end 
 
 
-get '/:query' do 
+get '/query/:text' do 
 
+   puts "Querying #{params[:text]}"
+   uri=URI.parse("http://text-processing.com/")
+   http=Net::HTTP.new(uri.host,uri.port)
+   request=Net::HTTP::Post.new("/api/phrases/")
+   request.add_field('Content-Type','application/json')
+   request.body=params[:text].to_s
+   response=http.request(request)
+   json_response= response.body
+   print json_response
+   
 
-end 
+end
 
 

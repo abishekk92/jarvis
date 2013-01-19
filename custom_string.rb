2bafa549db3982030cf35badd3e87d7ceb2080b2
Abishek Bhat <bhat.abishek@gmail.com>
2013-01-19 19:57:34 +0530

    modified:   app.rb Refactoring to accomodate to induced API changes 	new file:   classifier.rb Class skeleton for the classifier 	new file:   custom_string.rb Class skeleton for the custom_string class

---
+++ custom_string.rb
@@ -0,0 +1,22 @@
+class String
+	def is_weather?
+		return false
+	end 
+
+	def is_restaurant?
+		return false
+        end 
+	
+	def is_cricket?
+	       return false
+	end 
+
+	def is_football?
+		return false
+	end 	
+	
+	def is_command?
+		return false
+	end 
+end
+
